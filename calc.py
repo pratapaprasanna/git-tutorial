3257865160606c8c8723cd86caef4b13ef0dcc07
pratapagoutham@gmail.com <pratapagoutham@gmail.com>
2023-02-28 04:05:35 +0000

    Support adding three numbers

--- calc.py
+++ calc.py
@@ -13,8 +13,12 @@ def div(a,b):
 def sub(a,b):
     return a-b
 
+def sumnum(a, b, c):
+    return a+b+c
+
 
 print (add(19,25))
 print (multi(14, 18))
 print (div(56, 4))
 print (sub(65, 43))
+print (sumnum(45, 23, 34))
