2c8166fe6c0e0389b0896796dd80df7582af78dc
pratapagoutham@gmail.com <pratapagoutham@gmail.com>
2023-02-28 03:58:10 +0000

    fix indentation gaps

--- calc.py
+++ calc.py
@@ -1,15 +1,18 @@
-
 def add(a,b):
     return a+b
+
 
 def multi(a,b):
     return a*b
 
+
 def div(a,b):
     return a/b
 
+
 def sub(a,b):
     return a-b
+
 
 print (add(19,25))
 print (multi(14, 18))
